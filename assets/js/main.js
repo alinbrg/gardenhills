16ef13682f510961b59b9126842294b28e4d0322
alinbrg <alina.bregvadze@gmail.com>
2022-07-20 14:23:06 +0400

    main page blog texts

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,35 @@ function burgerMenu() {
 }
 
 burgerMenu();
+
+function mainPageBlogTexts() {
+	const mainTitle = document.querySelector(".main_article-title");
+	const titles = document.querySelectorAll(".article-title");
+	const articleAllP = document.querySelectorAll(
+		".article-desc p:first-of-type"
+	);
+
+	const articleImg = document.querySelector(".article-img");
+	const articleImgHeight = articleImg.getBoundingClientRect().height;
+
+	if (window.innerWidth < 1024) {
+		if (mainTitle.innerText.length > 80) {
+			const text = mainTitle.innerText;
+			mainTitle.innerText = text.slice(0, 80) + "...";
+		}
+
+		titles.forEach((el, i) => {
+			if (el.innerText.length > 70) {
+				const text = el.innerText;
+				el.innerText = text.slice(0, 70) + "...";
+			}
+			if (window.innerWidth < 768) {
+				if (el.getBoundingClientRect().height >= articleImgHeight / 2 - 20) {
+					articleAllP[i].style.display = "none";
+				}
+			}
+		});
+	}
+}
+
+mainPageBlogTexts();
